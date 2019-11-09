62032ab14f6ff16e01921cacede2d2feec78e1c3
rayacooper <47796461+rayacooper@users.noreply.github.com>
2019-11-09 10:47:31 -0700

    A level 7 Kata

---
+++ Level-7/Growing Plants.js
@@ -0,0 +1,39 @@
+// function growingPlant(upSpeed, downSpeed, desiredHeight) {
+//     return upSpeed > desiredHeight ? 1 : Math.floor(desiredHeight / (upSpeed - downSpeed));
+//   }
+
+ 
+
+// Test.assertEquals(growingPlant(100,10,910),10)
+
+// Test.assertEquals(growingPlant(10,9,4),1)
+
+function growingPlant(upSpeed, downSpeed, desiredHeight){
+    let x = 0; 
+    let ans;
+    for (let i = 1; i < desiredHeight; i++){
+        console.log(x)
+        x += upSpeed
+        console.log(x)
+        if (x >= desiredHeight){
+            ans = i;
+            break;
+        }else{
+            x -= downSpeed;
+        }
+    }
+    return ans;
+}
+
+console.log(growingPlant(10,9,4))
+
+// Best Practices Version:
+
+// function growingPlant(upSpeed, downSpeed, desiredHeight) {  
+//     var i = 1;
+//     for (var height = upSpeed; height < desiredHeight; height = height + upSpeed){
+//       height = height - downSpeed;
+//       i++;   
+//     }
+//     return i;
+//   }
