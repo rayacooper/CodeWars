2a7fdaa16589ee5b9a6a9c9adb99ee42fe53ccea
rayacooper <47796461+rayacooper@users.noreply.github.com>
2021-01-21 11:57:08 -0700

    Lvl 7 Kata

---
+++ Level-7/Simple Consecutive Pairs.js
@@ -0,0 +1,28 @@
+// In this Kata your task will be to return the count of pairs that have consecutive numbers as follows:
+
+// pairs([1,2,5,8,-4,-3,7,6,5]) = 3
+// The pairs are selected as follows [(1,2),(5,8),(-4,-3),(7,6),5]
+// --the first pair is (1,2) and the numbers in the pair are consecutive; Count = 1
+// --the second pair is (5,8) and are not consecutive
+// --the third pair is (-4,-3), consecutive. Count = 2
+// --the fourth pair is (7,6), also consecutive. Count = 3. 
+// --the last digit has no pair, so we ignore.
+// More examples in the test cases.
+
+// Good luck!
+
+
+// With this solution, I originally forgot to have the for loop as "i += 2"
+// and originally had it as "i + 2," which caused the function to fail and
+// for codewars.com to time out: 
+
+function pairs(ar){
+    let count = 0;
+    for(i = 0; i < ar.length; i+=2){
+        if(Math.abs(ar[i] - ar[i + 1]) === 1){
+            count++
+        }
+    }
+    return count;
+   };
+
