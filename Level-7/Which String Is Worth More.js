8a75851fcc5ef7e4a17f614845ace385dd91d9a0
unknown <sarah.jkr@gmail.com>
2021-09-24 12:56:24 -0600

    Lvl 7 kata

---
+++ Level-7/Which String Is Worth More.js
@@ -0,0 +1,17 @@
+// You will be given two ASCII strings, a and b. 
+// Your task is write a function to determine which one of these strings is "worth" more, and return it.
+
+// A string's worth is determined by the sum of its ASCII codepoint indexes. 
+// So, for example, the string HELLO has a value of 372: H is codepoint 72, E 69, L 76, and O is 79. The sum of these values is 372.
+
+// In the event of a tie, you should return the first string, i.e. a.
+
+// Hooray this one works! And isn't too long
+
+function highestValue(a, b) {
+    function innerFunc(e) {
+      return e.slice().split('').reduce((acc, elem) => acc + elem.charCodeAt(0),0)
+    }
+  
+  return innerFunc(a) < innerFunc(b) ? b : a;
+}
